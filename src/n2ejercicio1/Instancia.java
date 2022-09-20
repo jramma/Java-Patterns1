49fc959fe5501155172c8e68fae3b4f49bb72bee
Pepe <107991714+jramma@users.noreply.github.com>
2022-09-20 10:15:33 +0200

    Delete src/n2ejercicio1 directory

--- src/n2ejercicio1/Instancia.java
+++
@@ -1,7 +0,0 @@
-package n2ejercicio1;
-
-public interface Instancia {
-		public Phone writePhone();
-		public Adress writeAdress();
-		
-}
--- src/n2ejercicio1/ServicioFactory.java
+++
@@ -1,5 +0,0 @@
-package n2ejercicio1;
-
-public interface ServicioFactory {
-	public Instancia crearInstancia();
-}
