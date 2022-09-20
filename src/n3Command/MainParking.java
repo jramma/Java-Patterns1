f04b4e07a84fe70c73bbf06ca6d94cb207385df8
Pepe <107991714+jramma@users.noreply.github.com>
2022-09-20 15:38:06 +0200

    Update MainParking.java

--- src/n3Command/MainParking.java
+++ src/n3Command/MainParking.java
@@ -2,7 +2,7 @@ package n3Command;
 
 public class MainParking {
 	public static void main(String [] args) {
-		// solo he creado BreakBoat, RunBicylcle
+		// vehiculos disponibles "car" "bicycle" "plane" "boat"
 		Vehicle newVehicle = Parking.parking();
 		
 		Break_ breakCommand = new Break_(newVehicle);
