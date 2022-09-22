45ce78d410a36cb489eab2ff61beb2d4c5f9299f
Pepe <107991714+jramma@users.noreply.github.com>
2022-09-22 16:54:09 +0200

    Delete src/n3Command directory

--- src/n3Command/Vehicle.java
+++
@@ -1,7 +0,0 @@
-package n3Command;
-
-public interface Vehicle {
-	public void acelerate();
-	public void break_();
-	public void run();
-}
--- src/n3Command/VehicleComands.java
+++
@@ -1,6 +0,0 @@
-package n3Command;
-
-public interface VehicleComands {
-	public void execute();
-
-}
